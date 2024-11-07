8f9e7e693474a4c9e8ac2c82328ae9941e176fb7
samuelklimko1 <samuelklimko1@gmail.com>
2024-11-07 19:26:22 +0100

    eightcommit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react"
 import "./footer.css"
-
+import { Link } from "react-router-dom"
 
 function Footer() {
 
     return (
         <>
             <div className="container-footer">
-                footer
+                <img src="/webion.png" className="webion-footer"/>
+                <div className="container-quicklinks">
+                    <h5>Rýchle linky</h5>
+                    <Link to="https://www.instagram.com/webion.sk/" className="link-footer">Instagram</Link>
+                    <Link to="https://beautybytina.eu" className="link-footer">Beauty By Tina</Link>
+                    <Link to="https://vamoska.sk" className="link-footer">Vamoska</Link>
+                </div>
             </div>
         </>
     )
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -25,39 +25,31 @@ function Home() {
             </div>
             <div id="form" className="container-form">
                 <form className="contactForm">
-                    <label for="name">Meno:</label>
-                    <input type="text" id="name" name="name" required />
-
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
-
-                    <label for="phone">Telefónne číslo:</label>
-                    <input type="tel" id="phone" name="phone" required />
-
-                    <label for="details">Správa:</label>
-                    <textarea id="details" name="details" rows="5" placeholder="Popíšte ako si predstavujete vašu stránku: "></textarea>
-
-                    <div>
-                        <input type="checkbox" id="design" name="interest" value="design" checked disabled/>
-                        <label for="design">Web dizajn</label>
-
-                        <input type="checkbox" id="development" name="interest" value="development" checked disabled/>
-                        <label for="development">Web vývoj</label>
-
-                        <input type="checkbox" id="seo" name="interest" value="seo" checked disabled/>
-                        <label for="seo">SEO optimalizácia</label>
-
-                        <input type="checkbox" id="hosting-domena" name="interest" value="hosting-domena" checked disabled/>
-                        <label for="hosting-domena">Hosting a Doména</label>
-
-                        <input type="checkbox" id="eshop" name="interest" value="eshop"/>
-                        <label for="eshop">E-Shop</label>
-
-                        <input type="checkbox" id="booking" name="interest" value="booking"/>
-                        <label for="booking">Booking</label>
+                    <div className="container-iandt-form">
+                        <div className="container-inputs-form">
+                            <input type="text" id="name" name="name" placeholder="Meno: " className="input-form" required />
+                            <input type="email" id="email" name="email" placeholder="Email: " className="input-form" required />
+                            <input type="tel" id="phone" name="phone" placeholder="Telefónne číslo: " className="input-form" required />
+                        </div>
+                        <textarea id="details" name="details" rows="5" placeholder="Popíšte ako si predstavujete vašu stránku: "className="input-form textarea-form"/>
                     </div>
-
-                    <input type="submit" onSubmit="" />
+                    <div className="container-checkbox-form">
+                        <input type="checkbox" id="design" name="interest" value="design" className="checkbox-form" checked disabled/>
+                        <label for="design">Web dizajn</label>
+                        <input type="checkbox" id="development" name="interest" value="development" className="checkbox-form" checked disabled/>
+                        <label for="development">Web vývoj</label>
+                        <input type="checkbox" id="hosting-domena" name="interest" value="hosting-domena" className="checkbox-form" checked disabled/>
+                        <label for="hosting-domena">Hosting a Doména</label>
+                        <input type="checkbox" id="eshop" name="interest" value="eshop" className="checkbox-form" />
+                        <label for="eshop">E-Shop</label>
+                        <input type="checkbox" id="booking" name="interest" value="booking" className="checkbox-form" />
+                        <label for="booking">Booking</label>
+                        <input type="checkbox" id="seo" name="interest" value="seo" className="checkbox-form"/>
+                        <label for="seo">SEO optimalizácia</label>
+                    </div>
+                    <div className="container-submit-form">
+                        <button type="submit" onSubmit="" className="form-submit-button">Odoslať</button>
+                    </div>
                 </form>
             </div>
         </>
